309784f4a90d0da60b1b42d70fda2864f44933ef
Benjamín Peralta <benjaminp158@gmail.com>
2024-09-13 17:09:39 -0300

    Añadido lógica de físicas

--- player.js
+++ player.js
@@ -3,20 +3,36 @@ const player = {
         x: 0,
         y: 0
     },
+    speed: 0,
     move: {
         right: function(){
-            player.position.x += 1
+            player.position.x += 1 * player.speed
         },
         left: function(){
-            player.position.x -= 1
+            player.position.x -= 1 * player.speed
         },
         up: function() {
-            player.position.y -= 1
+            player.position.y -= 1 * player.speed
         },
         down: function() {
-            player.position.y += 1
+            player.position.y += 1 * player.speed
         }
+    },
+    physics: {
+        gravityScale: 1,
+        applyGravity: function(){
+            player.position.y += 1 * player.physics.gravityScale
+        },
+        forceToBeOnTop() {
+            if(player.isGrounded()) {
+                player.position. y = 115
+            }
+        }
+    },
+    isGrounded: function(){
+        return player.position.y >= 115
     }
+
 }
 
 export default player
